8159bd22ad0e8606cd99da7a8ec57bced94b764e
Gu1paz <guilhermepaz520@gmail.com>
2026-09-05 14:43:25 -0300

    alternado o main

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
+import tkinter as tk
 from app.core.database import Database
+from app.dao.ordem_servico_dao import OrdemServicoDAO
+from app.view.ordem_servico_view import OrdemServicoView
 
-from app.view.ordem_servico_view import Ordem_servico_View
-from app.dao.ordem_servico_dao import Ordem_servico_DAO
-
-class ErpAplication:
+class ErpApplication:
     def __init__(self):
-
-        init(autoreset=True)
-
         self._database = Database()
-
+        self._ordem_servico_dao = OrdemServicoDAO(self._database)
+        
         self._root = tk.Tk()
+        self._root.title("Sistema ERP - Assistência Técnica")
+        self._root.geometry("800x600")
 
-        self._janela_ordem_servico = Ordem_servico_View(self._root, self._database)
+        # Passa a DAO para a View, e não a conexão do banco bruta
+        self._janela_ordem_servico = OrdemServicoView(self._root, self._ordem_servico_dao)
+
+    def run(self):
+        self._root.mainloop()
+
+if __name__ == "__main__":
+    app = ErpApplication()
+    app.run()

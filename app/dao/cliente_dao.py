85e0786c53e563dc77c51703b58c30edc789803f
Gu1paz <guilhermepaz520@gmail.com>
2026-09-26 16:47:34 -0300

    refactor: alterando toda a DAO

--- app/dao/cliente_dao.py
+++ app/dao/cliente_dao.py
@@ -1,178 +1,22 @@
-from app.dao.dao import DAO
+from app.dao.simple_dao import SimpleDAO
 from app.models.cliente import Cliente
 
-class Cliente_DAO(DAO):
+
+class ClienteDAO(SimpleDAO):
     def __init__(self, database):
-        super().__init__(database)
-
-    def save(self, cliente):
-        conexao, cursor = self.conectar()
-        try: 
-            sql = """
-                    INSERT INTO CLIENTES
-                    (
-                        NOME,
-                        CPF,
-                        TELEFONE,
-                        EMAIL
-                    )
-                    VALUES
-                    (
-                        %s,
-                        %s,
-                        %s,
-                        %s
-                    )
-                  """
-            cursor.execute(
-                sql,
-                (
-                    cliente.nome,
-                    cliente.cpf,
-                    cliente.telefone,
-                    cliente.email
-                )
-            )
-            
-            conexao.commit()
-            
-            cliente.id = cursor.lastrowid
-
-            return cliente
-        
-        except Exception:
-            conexao.rollback()
-            raise
-
-        finally:
-            self.desconectar(cursor, conexao)
+        super().__init__(
+            database,
+            tabela="clientes",
+            colunas=["nome", "cpf", "telefone", "email"],
+            construtor=Cliente,
+            para_tupla=lambda cliente: (
+                cliente.nome,
+                cliente.cpf,
+                cliente.telefone,
+                cliente.email,
+            ),
+        )
 
     def get_all(self):
-        conexao, cursor = self.conectar()
-        try:
-            sql = """
-                    SELECT
-                        ID,
-                        NOME,
-                        CPF,
-                        TELEFONE,
-                        EMAIL
-                    FROM 
-                        CLIENTES
-                    ORDER BY
-                        NOME
-                  """
-            
-            cursor.execute(sql)
-
-            registros = cursor.fetchall()
-
-            clientes = []
-
-            for registro in registros:
-                clientes.append(
-                    Cliente( 
-                    registro[0],
-                    registro[1],
-                    registro[2],
-                    registro[3],
-                    registro[4]
-                    )
-                )
-            return clientes
-        
-        finally:
-            self.desconectar(cursor, conexao)
-
-    def get_by_id(self, id):
-        conexao, cursor = self.conectar()
-        try:
-            sql = """
-                    SELECT
-                        ID,
-                        NOME,
-                        CPF,
-                        TELEFONE,
-                        EMAIL
-                    FROM
-                        CLIENTES
-                    WHERE
-                        ID = %s
-                  """
-            
-            cursor.execute(sql, (id,))
-
-            registro = cursor.fetchone()
-
-            if registro is None:
-                return None
-            
-            return Cliente(
-                registro[0],
-                registro[1],
-                registro[2],
-                registro[3],
-                registro[4]
-            )
-        
-        finally:
-            self.desconectar(cursor, conexao)
-
-    def update(self, cliente):
-        conexao, cursor = self.conectar()
-        try:
-            sql = """
-                    UPDATE CLIENTES
-                    SET
-                        NOME = %s,
-                        CPF = %s,
-                        TELEFONE = %s,
-                        EMAIL = %s
-                    WHERE
-                        ID = %s
-                  """
-            
-            cursor.execute(
-                sql,
-                (
-                    cliente.nome,
-                    cliente.cpf,
-                    cliente.telefone,
-                    cliente.email,
-                    cliente.id
-                )
-            )
-
-            conexao.commit()
-
-            return cursor.rowcount > 0
-        
-        except Exception:
-
-            conexao.rollback()
-            raise
-
-        finally:
-            self.desconectar(cursor, conexao)
-
-    def delete(self, id):
-        conexao, cursor = self.conectar()
-        try:
-            sql ="""
-                    DELETE
-                    FROM CLIENTES
-                    WHERE ID = %s
-                 """
-            
-            cursor.execute(sql, (id,))
-
-            conexao.commit()
-
-            return cursor.rowcount > 0
-        
-        except Exception:
-            conexao.rollback()
-            raise
-
-        finally:
-            self.desconectar(cursor, conexao)
+        # mantém o comportamento original: lista sempre ordenada por nome
+        return super().get_all(order_by="nome")
--- app/dao/funcionario_dao.py
+++ app/dao/funcionario_dao.py
@@ -1,160 +1,21 @@
-from app.dao.dao import DAO
+from app.dao.simple_dao import SimpleDAO
 from app.models.funcionario import Funcionario
 
-class Funcionario_DAO(DAO):
+
+class FuncionarioDAO(SimpleDAO):
     def __init__(self, database):
-        super().__init__(database)
-
-    def save(self, funcionario):
-        conexao, cursor = self.conectar()
-        try: 
-            sql = """
-                    INSERT INTO FUNCIONARIOS
-                    (
-                        NOME,
-                        CPF,
-                        CARGO
-                    )
-                    VALUES
-                    (
-                        %s,
-                        %s,
-                        %s
-                    )
-                  """
-            cursor.execute(
-                sql,
-                (
-                    funcionario.nome,
-                    funcionario.cpf,
-                    funcionario.cargo
-                )
-            )
-            
-            conexao.commit()
-            
-            funcionario.id = cursor.lastrowid
-
-            return funcionario
-        
-        except Exception:
-            conexao.rollback()
-            raise
-
-        finally:
-            self.desconectar(cursor, conexao)
+        super().__init__(
+            database,
+            tabela="funcionarios",
+            colunas=["nome", "cpf", "cargo"],
+            construtor=Funcionario,
+            para_tupla=lambda funcionario: (
+                funcionario.nome,
+                funcionario.cpf,
+                funcionario.cargo,
+            ),
+        )
 
     def get_all(self):
-        conexao, cursor = self.conectar()
-        try:
-            sql = """
-
-                    SELECT
-                        ID,
-                        NOME,
-                        CPF,
-                        CARGO
-                    FROM 
-                        FUNCIONARIOS
-                    ORDER BY
-                        NOME
-                  """
-            
-            cursor.execute(sql)
-
-            registros = cursor.fetchall()
-
-            funcionarios = []
-
-            for registro in registros:
-                funcionarios.append(
-                    Funcionario( 
-                    registro[0],
-                    registro[1],
-                    registro[2],
-                    registro[3]
-                    )
-                )
-            return funcionarios
-        
-        finally:
-            self.desconectar(cursor, conexao)   
-
-    def get_by_id(self, id):
-        conexao, cursor = self.conectar()
-        try:
-            sql = """
-                    SELECT
-                        ID,
-                        NOME,
-                        CPF,
-                        CARGO
-                    FROM 
-                        FUNCIONARIOS
-                    WHERE
-                        ID = %s
-                  """
-            
-            cursor.execute(sql, (id,))
-
-            registro = cursor.fetchone()
-
-            if registro:
-                return Funcionario(
-                    registro[0],
-                    registro[1],
-                    registro[2],
-                    registro[3]
-                )
-            else:
-                return None
-        
-        finally:
-            self.desconectar(cursor, conexao)
-
-    def update(self, funcionario):
-        conexao, cursor = self.conectar()
-        try:
-            sql ="""
-                   UPDATE FUNCIONARIOS
-                   SET
-                        NOME = %s,
-                        CPF = %s,
-                        CARGO = %s
-                   WHERE
-                        ID = %s
-                 """
-            cursor.execute(
-                sql,
-                (
-                    funcionario.nome,
-                    funcionario.cpf,
-                    funcionario.cargo,
-                    funcionario.id
-                )
-            )
-            conexao.commit()
-            sucesso = cursor.rowcount > 0
-            return sucesso
-        except Exception:
-            conexao.rollback()
-            raise
-        finally:
-            self.desconectar(cursor, conexao)
-
-    def delete(self, id):
-        conexao, cursor = self.conectar()
-        try:
-            sql = """
-                    DELETE FROM FUNCIONARIOS
-                    WHERE ID = %s
-                  """
-            cursor.execute(sql, (id,))
-            conexao.commit()
-            sucesso = cursor.rowcount > 0
-            return sucesso
-        except Exception:
-            conexao.rollback()
-            raise
-        finally:
-            self.desconectar(cursor, conexao)
+        # mantém o comportamento original: lista ordenada por nome
+        return super().get_all(order_by="nome")
